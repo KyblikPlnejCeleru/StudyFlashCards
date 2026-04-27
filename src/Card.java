d78dc7b26e82d1cca29ad4431c1ba708abc1ec41
Roman Potoček <potocek@SPSEJECNANET.CZ>
2026-04-27 12:43:20 +0200

    Added class Card

---
+++ src/Card.java
@@ -0,0 +1,21 @@
+import java.awt.*;
+
+public class Card {
+
+    private String name;
+    private int id;
+    private String imageName;
+    private Question question;
+
+    public Card(String name, int id, String imageName, Question question) {
+        this.name = name;
+        this.id = id;
+        this.imageName = imageName;
+        this.question = question;
+    }
+
+
+
+
+
+}
