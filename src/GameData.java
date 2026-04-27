ae5f33e92e4c0c3af7189325b2532d70e7202db9
legenda romek <potocek@spsejecna.cz>
2026-04-27 12:46:09 +0200

    Started working on gameData.. - to early there is nothing working but its atleast ready to start when im finished with all the other classes that im currently working on.

---
+++ src/GameData.java
@@ -0,0 +1,62 @@
+
+
+import com.sun.tools.javac.Main;
+
+import com.google.gson.Gson;
+
+
+import java.io.FileReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.nio.charset.StandardCharsets;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+/**
+ *
+ * Thahle trida slouzi pro nacitani hernich dat z jsonu, napriklad postavy,predmety a celkova mapa.
+ * @author romek studios + meitnerova + chaloupka + kolinek + pospisil
+ */
+public class GameData {
+
+    public ArrayList<Card> card;
+
+    public ArrayList<Card> getCards() {
+        return card;
+    }
+
+    public void setPredmety(ArrayList<Card> card) {
+        this.card = card;
+    }
+
+    public ArrayList<Card> getCard() {
+        return card;
+    }
+    /**
+     * nacita herni data z jsonu
+     *
+     * @param resourcePath cesta k jsonu
+     * @return herni data z jsonu
+     */
+    public static GameData loadGameDataFromResources(String resourcePath) {
+        //Vytvoření objektu pro práci s JSON souborem
+        Gson gson = new Gson();
+
+        //Načtení souboru gamedata.json, musí být ve složce res/resources, ta musí být označena jako resource složka projektu
+
+        InputStream input = GameData.class.getResourceAsStream("/GameData.json");
+        if (input == null){
+            throw new RuntimeException();
+        }
+        try (input) {
+            //Přečte celý JSON a vytvoří instanci GameData, naplní vlastnosti podle názvů klíčů v JSONU, vrátí se hotová třída GameData
+            return gson.fromJson(new InputStreamReader(input, StandardCharsets.UTF_8), GameData.class);
+
+        } catch (Exception e) {
+            throw new RuntimeException("Chyba při načítání JSON: " + e.getMessage());
+        }
+
+    }
+
+}
