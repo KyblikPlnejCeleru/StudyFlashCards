e64403052626be51e0eb1a1237bc648a442aa81c
romek <potocek@spsejecna.cz>
2026-05-05 17:33:16 +0200

    Made it so the image is loaded into the center of the GameScreen

--- src/GameScreen.java
+++ src/GameScreen.java
@@ -1,3 +1,4 @@
+import javax.imageio.plugins.jpeg.JPEGImageReadParam;
 import javax.swing.*;
 import java.awt.*;
 import java.util.ArrayList;
@@ -21,6 +22,20 @@ public class GameScreen {
         frame.setLayout(new BorderLayout());
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
+
+        JButton answer1 = new JButton("a");
+        JButton answer2 = new JButton("b");
+        JButton answer3 = new JButton("c");
+        JButton answer4 = new JButton("d");
+
+        ImageIcon img = new ImageIcon("test.jpg");
+        JLabel imageLabel = new JLabel();
+        imageLabel.setIcon(new ImageIcon(img.getImage().getScaledInstance(600, 600, Image.SCALE_SMOOTH)));
+        imageLabel.setHorizontalAlignment(JLabel.CENTER);
+        imageLabel.setVerticalAlignment(JLabel.CENTER);
+        ;//""
+        frame.add(imageLabel,BorderLayout.CENTER);
+
 
 
     }
