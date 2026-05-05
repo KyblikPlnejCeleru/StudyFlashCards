300620399ac7c9d2ed6fb8a7a4a5275388b8efc3
romek <potocek@spsejecna.cz>
2026-05-05 18:38:29 +0200

    Added Game.java to store users data.

--- src/Game.java
+++ src/Game.java
@@ -1,2 +1,35 @@
 public class Game {
+
+    private int correctAnsw;
+    private int wrongAns;
+
+    public Game() {
+        this.correctAnsw =0;
+        this.wrongAns =0;
+    }
+
+    public int numberOfAnswers(){
+     return correctAnsw+wrongAns;
+    }
+
+
+    public int getCorrectAnsw() {
+        return correctAnsw;
+    }
+
+    public void setCorrectAnsw(int correctAnsw) {
+        this.correctAnsw = correctAnsw;
+    }
+
+    public int getWrongAns() {
+        return wrongAns;
+    }
+
+    public void setWrongAns(int wrongAns) {
+        this.wrongAns = wrongAns;
+    }
+
+
+
+
 }
