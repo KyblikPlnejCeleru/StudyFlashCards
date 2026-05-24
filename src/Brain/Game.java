2cd1bd62d142b25f3f627a8d0f17e3bc0d85adc8
romek <potocek@spsejecna.cz>
2026-05-24 11:47:38 +0200

    Madde it better

--- src/Brain/Game.java
+++ src/Brain/Game.java
@@ -1,9 +1,14 @@
 package Brain;
 
+/**
+ * The Game class manages the game's statistics, such as the number of correct and wrong answers.
+ */
 public class Game {
+
 
     private int correctAnsw;
     private int wrongAns;
+
 
     public Game() {
         this.correctAnsw =0;
@@ -13,30 +18,26 @@ public class Game {
     public int numberOfAnswers(){
      return correctAnsw+wrongAns;
     }
-
-
     public int getCorrectAnsw() {
         return correctAnsw;
     }
-
     public void setCorrectAnsw(int correctAnsw) {
         this.correctAnsw = correctAnsw;
     }
-
     public int getWrongAns() {
         return wrongAns;
     }
-
     public void setWrongAns(int wrongAns) {
         this.wrongAns = wrongAns;
     }
 
 
-    public double succesPercentage(double cardCount){
-        return  Math.round(correctAnsw/cardCount*100);
+    /**
+     * Calculates the success percentage based on the number of correct answers and total cards.
+     *
+     * @return The success percentage, rounded to the nearest whole number.
+     */
+    public double succesPercentage(){
+        return  Math.round((double) correctAnsw /numberOfAnswers()*100);
     }
-
-
-
-
 }
